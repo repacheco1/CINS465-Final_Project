2341f855e252b8fd9dbe10722e7e2b2640981876
Roberto E Pacheco <repacheco1@mail.csuchico.edu>
2020-04-13 21:06:38 -0700

    Revert backend

--- mysite/foodfficient/forms.py
+++ mysite/foodfficient/forms.py
@@ -2,11 +2,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
-from foodfficient.models import Profile
-from django.forms import modelformset_factory, formset_factory
-from django.db import models
-from .models import (Recipe, RequiredIngredients, OptionalIngredients, Substitutions, Instructions)
 
 def must_be_unique(value):
     user = User.objects.filter(email=value)
@@ -15,104 +10,6 @@ def must_be_unique(value):
     # Always return the cleaned data, whether you have changed it or
     # not.
     return value
-
-class RecipeModelForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = ('recipe_name', 'recipe_picture', 'recipe_time',)
-        labels = {
-            'recipe_name': 'Recipe name:',
-            'recipe_picture': 'Recipe picture:', 
-            'recipe_time': 'Recipe cooking time (minutes):',
-        }
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter recipe name here'
-                }
-            ),
-            'name': forms.FileInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Choose recipe image...'
-                }
-            ),
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter cooking time here:'
-                }
-            ),
-        }
-
-RequiredIngredientsFormset = modelformset_factory(
-    RequiredIngredients,
-    fields=('required_ingredient', ),
-    extra=1,
-    widgets={
-        'required_ingredient': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter required ingredient here:'
-            }
-        )
-    }
-)
-
-OptionalIngredientsFormset = modelformset_factory(
-    OptionalIngredients,
-    fields=('optional_ingredient', ),
-    extra=1,
-    widgets={
-        'optional_ingredient': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter optional ingredient here:'
-            }
-        )
-    }
-)
-
-SubstitutionsFormset = modelformset_factory(
-    Substitutions,
-    fields=('substitutions', ),
-    extra=1,
-    widgets={
-        'substitutions': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter substitution here:'
-            }
-        )
-    }
-)
-
-InstructionsFormset = modelformset_factory(
-    Instructions,
-    fields=('required_instructions', 'optional_instructions', 'substitution_instructions', ),
-    extra=1,
-    widgets={
-        'required_instructions': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter required instructions here:'
-            }
-        ),
-        'optional_instructions': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter optional instructions here:'
-            }
-        ),
-        'substitution_instructions': forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter substitution instructions here:'
-            }
-        ),
-    }
-)
-
-
-
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(
@@ -132,13 +29,3 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('bio', 'location', 'avatar')
--- mysite/foodfficient/views.py
+++ mysite/foodfficient/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.views import generic
-from django.db import transaction
-from . import forms, models
-# from models import Recipe, RequiredIngredients, OptionalIngredients, Substitutions, Instructions
-from .forms import (
-    RecipeModelForm,
-    RequiredIngredientsFormset,
-    OptionalIngredientsFormset,
-    SubstitutionsFormset,
-    InstructionsFormset
-)
-from .models import (
-    Recipe,
-    RequiredIngredients,
-    OptionalIngredients,
-    Substitutions,
-    Instructions
-)
 
 def logoutPageView(request):
     logout(request)
@@ -76,81 +58,23 @@ def aboutPageView(request):
     }
     return render(request, "about.html", context=aboutContext)
 
-class RecipeListView(generic.ListView):
-    model: Recipe
-    context_object_name = 'recipe'
-    template_name="recipe.html"
+def recipePageView(request):
+    aboutContext = {
+        "title":"Recipes - Foodfficient",
+        "pageTitle":"Here is a list of all recipes on Foodfficient!",
+        "body":"",
+        "body2":"",
+    }
+    return render(request, "recipes.html", context=aboutContext)
+
 
 @login_required
 
 def addRecipePageView(request):
-    if request.method == 'GET':
-        recipeForm = RecipeModelForm(request.GET or None)
-        requiredForm = RequiredIngredientsFormset(queryset=RequiredIngredients.objects.none())
-        optionalForm = OptionalIngredientsFormset(queryset=OptionalIngredients.objects.none())
-        substitutionsForm = SubstitutionsFormset(queryset=Substitutions.objects.none())
-        instructionsForm = InstructionsFormset(queryset=Instructions.objects.none())
-    elif request.method == 'POST':
-        recipeForm = RecipeModelForm(request.POST)
-        requiredForm = RequiredIngredientsFormset(request.POST)
-        optionalForm = OptionalIngredientsFormset(request.POST)
-        substitutionsForm = SubstitutionsFormset(request.POST)
-        instructionsForm = InstructionsFormset(request.POST)
-    if recipeForm.is_valid() and requiredForm.is_valid() and optionalForm.is_valid() and substitutionsForm.is_valid() and instructionsForm.is_valid():
-        recipe = recipeForm.save()
-        for form in requiredForm:
-            ingredient = form.save(commit=False)
-            ingredient.recipe = recipe
-            ingredient.save()
-        for form in optionalForm:
-            optional = form.save(commit=False)
-            optional.recipe = recipe
-            optional.save()
-        for form in substitutionsForm:
-            subs = form.save(commit=False)
-            subs.recipe = recipe
-            subs.save()
-        for form in instructionsForm:
-            ins = form.save(commit=False)
-            ins.recipe = recipe
-            ins.save()
-        return redirect('Recipes')
-    addContext = {
-        'recipe': recipeForm,
-        'requiredForm': requiredForm,
-        'optional': optionalForm,
-        'substitutios': substitutionsForm,
-        'instructions': instructionsForm,
-        "title":"Add Recipe - Foodfficient",
-        "pageTitle":"Welcome to add a recipe Foodfficient!",
+    recipeContext = {
+        "title":"Recipe - Foodfficient",
+        "pageTitle":"Add a recipe to  Foodfficient!",
         "body":"",
         "body2":"",
     }
-    return render(request, "addRecipe.html", context=addContext)
-
-
-# @transaction.atomic
-# def profilePageView(request):
-#     if request.method == 'POST':
-#         user_form = UserForm(request.POST, instance=request.user)
-#         profile_form = ProfileForm(request.POST, instance=request.user.profile)
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#             messages.success(request, _('Your profile was successfully updated!'))
-#             return redirect('/profile/')
-#         else:
-#             messages.error(request, _('Please correct the error below.'))
-#     else:
-#         user_form = UserForm(instance=request.user)
-#         profile_form = ProfileForm(instance=request.user.profile)
-#     profileContext = {
-#         "title":"Profile - Foodfficient",
-#         "pageTitle":"Welcome to profiles Foodfficient!",
-#         "body":"",
-#         "body2":"",
-#     }
-#     return render(request, "profile.html", {
-#         'user_form': user_form,
-#         'profile_form': profile_form
-#     }, context=profileContext)
+    return render(request, "addRecipe.html", context=recipeContext)
