b68d03c3343068ab606359a1bb7170911e7e0629
Roberto E Pacheco <repacheco1@mail.csuchico.edu>
2020-04-18 14:47:39 -0700

    Fix mobile view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yj7s7w4w5eq8fp+!12zq#)y=!1cop5^9bv@ff-t2pi==7es&py'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.10','localhost',]
 
 STATIC_URL = '/static/'
 
